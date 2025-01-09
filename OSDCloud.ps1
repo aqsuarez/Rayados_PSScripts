172eccce7473de643d732afddf89836f474fcf96
aqsuarez <104507011+aqsuarez@users.noreply.github.com>
2025-01-09 12:37:51 -0600

    Update OSDCloud.ps1

--- OSDCloud.ps1
+++ OSDCloud.ps1
@@ -4,8 +4,8 @@ Write-Host "===================== Main Menu =======================" -Foreground
 Write-Host "================ www.brookspeppin.com =================" -ForegroundColor Yellow
 Write-Host "====================@brookspeppin =====================" -ForegroundColor Yellow
 Write-Host "=======================================================" -ForegroundColor Yellow
-Write-Host "1: Zero-Touch Win10 21H1 | English | Enterprise"-ForegroundColor Yellow
-Write-Host "2: Zero-Touch Win10 20H2 | English | Enterprise" -ForegroundColor Yellow
+Write-Host "1: Zero-Touch Win11 23H2 | English | Enterprise"-ForegroundColor Yellow
+Write-Host "2: Zero-Touch Win11 23H2 | Spanish | Enterprise" -ForegroundColor Yellow
 Write-Host "3: I'll select it myself"-ForegroundColor Yellow
 Write-Host "4: Exit`n"-ForegroundColor Yellow
 $input = Read-Host "Please make a selection"
@@ -17,8 +17,8 @@ Install-Module OSD -Force
 
 switch ($input)
 {
-    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 21H1 -OSEdition Enterprise -ZTI } 
-    '2' { Start-OSDCloud -OSLanguage en-us -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
+    '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 23H2 -OSEdition Enterprise -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage es-Mx -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
     '3' { Start-OSDCloud	} 
     '4' { Exit		}
 }
