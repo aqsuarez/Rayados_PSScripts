276fe7c7b97cb5d50531ef931e75a3ed3f63bf58
aqsuarez <104507011+aqsuarez@users.noreply.github.com>
2025-01-09 14:41:42 -0600

    Update OSDCloud.ps1

--- OSDCloud.ps1
+++ OSDCloud.ps1
@@ -6,8 +6,10 @@ Write-Host "====================@asuarez =====================" -ForegroundColor
 Write-Host "=======================================================" -ForegroundColor Yellow
 Write-Host "1: Zero-Touch Win11 23H2 | English | Enterprise"-ForegroundColor Yellow
 Write-Host "2: Zero-Touch Win11 23H2 | Spanish | Enterprise" -ForegroundColor Yellow
-Write-Host "3: I'll select it myself"-ForegroundColor Yellow
-Write-Host "4: Exit`n"-ForegroundColor Yellow
+Write-Host "3: Zero-Touch Win11 23H2 | French | Enterprise" -ForegroundColor Yellow
+Write-Host "4: Zero-Touch Win11 23H2 | Portuguese | Enterprise" -ForegroundColor Yellow
+Write-Host "5: I'll select it myself"-ForegroundColor Yellow
+Write-Host "6: Exit`n"-ForegroundColor Yellow
 $input = Read-Host "Please make a selection"
 
 Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
@@ -19,8 +21,10 @@ switch ($input)
 {
     '1' { Start-OSDCloud -OSLanguage en-us -OSBuild 23H2 -OSEdition Enterprise -ZTI } 
     '2' { Start-OSDCloud -OSLanguage es-Mx -OSBuild 23H2 -OSEdition Enterprise -ZTI } 
-    '3' { Start-OSDCloud	} 
-    '4' { Exit		}
+    '3' { Start-OSDCloud -OSLanguage fr-CA -OSBuild 23H2 -OSEdition Enterprise -ZTI } 
+    '4' { Start-OSDCloud -OSLanguage pt-BR -OSBuild 23H2 -OSEdition Enterprise -ZTI } 
+    '5' { Start-OSDCloud	} 
+    '6' { Exit		}
 }
 
 wpeutil reboot
