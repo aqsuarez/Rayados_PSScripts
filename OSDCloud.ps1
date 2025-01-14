eb46eb3e2412431deaffa63f374f09dcd302d801
aqsuarez <104507011+aqsuarez@users.noreply.github.com>
2025-01-14 09:16:12 -0600

    Update OSDCloud.ps1

--- OSDCloud.ps1
+++ OSDCloud.ps1
@@ -1,15 +1,16 @@
 ﻿Write-Host  -ForegroundColor Yellow "Starting Rayados' Custom OSDCloud ..."
 cls
 Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
-Write-Host "================ www.Rayados.pro =================" -ForegroundColor Yellow
-Write-Host "====================@asuarez =====================" -ForegroundColor Yellow
+Write-Host "================== www.Rayados.pro ====================" -ForegroundColor Yellow
+Write-Host "=======================@asuarez =======================" -ForegroundColor Yellow
 Write-Host "=======================================================" -ForegroundColor Yellow
-Write-Host "1: Zero-Touch Win11 23H2 | English | Enterprise"-ForegroundColor Yellow
-Write-Host "2: Zero-Touch Win11 23H2 | Spanish | Enterprise" -ForegroundColor Yellow
-Write-Host "3: Zero-Touch Win11 23H2 | French | Enterprise" -ForegroundColor Yellow
+Write-Host "1: Zero-Touch Win11 23H2 | English    | Enterprise"-ForegroundColor Yellow
+Write-Host "2: Zero-Touch Win11 23H2 | Spanish    | Enterprise" -ForegroundColor Yellow
+Write-Host "3: Zero-Touch Win11 23H2 | French     | Enterprise" -ForegroundColor Yellow
 Write-Host "4: Zero-Touch Win11 23H2 | Portuguese | Enterprise" -ForegroundColor Yellow
-Write-Host "5: I'll select it myself"-ForegroundColor Yellow
-Write-Host "6: Exit`n"-ForegroundColor Yellow
+Write-Host "5: Azure OSDCloud        | Portuguese | Enterprise" -ForegroundColor Yellow
+Write-Host "6: I'll select it myself"-ForegroundColor Yellow
+Write-Host "7: Exit`n"-ForegroundColor Yellow
 $input = Read-Host "Please make a selection"
 
 Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
@@ -23,8 +24,9 @@ switch ($input)
     '2' { Start-OSDCloud -OSLanguage es-Mx -OSBuild 23H2 -OSEdition Enterprise -ZTI } 
     '3' { Start-OSDCloud -OSLanguage fr-CA -OSBuild 23H2 -OSEdition Enterprise -ZTI } 
     '4' { Start-OSDCloud -OSLanguage pt-BR -OSBuild 23H2 -OSEdition Enterprise -ZTI } 
-    '5' { Start-OSDCloud	} 
-    '6' { Exit		}
+    '5' { powershell iex (irm az.osdcloud.com) -ZTI } 
+    '6' { Start-OSDCloud	} 
+    '7' { Exit		}
 }
 
 wpeutil reboot
